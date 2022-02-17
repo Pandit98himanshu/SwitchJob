3f5a0c886c63af10385b8cc79f37e55f106bc89f
Himanshu Shekhar <pandit98himanshu@gmail.com>
2022-02-18 01:01:07 +0530

    solving striver's DSA cracker sheet

---
+++ striversheet/arrays/PascalsTriangle.java
@@ -0,0 +1,42 @@
+package striversheet.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created at : 18/02/22
+ * <p>
+ * <a href=https://leetcode.com/problems/pascals-triangle/>118. Pascal's Triangle</a>
+ *
+ * @author Himanshu Shekhar
+ */
+
+public class PascalsTriangle {
+    /**
+     * Add corresponding values of previous rows and generate current row
+     * <p>Time Complexity: O(numRows<sup>2</sup>); 0 ms, faster than 100.00%
+     * <br>Space Complexity: O(numRows<sup>2</sup>)
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        res.add(row);
+
+        if (numRows == 1)
+            return res;
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = row;
+            row = new ArrayList<>();
+            row.add(1);     // 1 is there at the beginning of every row
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+            row.add(1);     // 1 is there at the end of every row
+
+            res.add(row);
+        }
+        return res;
+    }
+}
---
+++ striversheet/arrays/SetMatrixZeroes.java
@@ -0,0 +1,49 @@
+package striversheet.arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created at : 17/02/22
+ * <p>
+ * <a href=https://leetcode.com/problems/set-matrix-zeroes/>73. Set Matrix Zeroes</a>
+ *
+ * @author Himanshu Shekhar
+ */
+
+public class SetMatrixZeroes {
+    /**
+     * The idea is to use hashset to remove repetition.
+     * <p>Time Complexity: O(2 * (m + n)); 1 ms, faster than 93.63 %
+     * <br>Space Complexity: O(m + n); 43.5 MB, less than 72.97%
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;  // number of rows
+        int n = matrix[0].length;   // number of columns
+        Set<Integer> rows = new HashSet<>();    // set contains row numbers having "0"
+        Set<Integer> cols = new HashSet<>();    // set contains column numbers having "0"
+
+        // iterate whole matrix and store
+        // row & column numbers having "0"s
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        // make the whole row "0"
+        for (int row : rows) {
+            Arrays.fill(matrix[row], 0);
+        }
+        // make the whole column "0"
+        for (int col : cols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+}
